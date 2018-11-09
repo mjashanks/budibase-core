9528e4ea670e6642105a0a38903401895d86eac5
Michael Shanks <mjashanks@hotmail.com>
2018-11-09 21:30:30 +0000

    actions started

---
+++ src/templateApi/createActions.js
@@ -0,0 +1,30 @@
+import {isUndefined} from "lodash/fp";
+import {isNothing} from "../common";
+
+export const createTrigger = () => ({
+    actionName: "",
+    eventName: "",
+    // function, has access to event context, 
+    // returns object that is used as parameter to action
+    // only used if triggered by event
+    paramsCreator: "",
+    // action runs if true, 
+    // has access to event context 
+    condition: "" 
+})
+
+export const createAction = () => ({
+    name: "", 
+    behaviourSource: "", 
+    // name of function in actionSource
+    behaviourName: "",    
+});
+
+export const validateActions = (allActions) => {
+
+};
+
+export const validateTrigger = (allTriggers, allActions) => {
+
+};
+
