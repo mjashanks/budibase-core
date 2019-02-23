a61ae11994349ff2837f62578234ca5483732919
Michael Shanks <mjashanks@hotmail.com>
2019-02-23 20:41:29 +0000

    validateUses tests

--- src/authApi/validateUser.js
+++ src/authApi/validateUser.js
@@ -1,10 +1,8 @@
-import {applyRuleSet, makerule, applyRule} from "../common/validationCommon";
-import {permissionTypes, WHITELIST, BLACKLIST} from "./authCommon";
-import {values, includes, map, concat, isEmpty, uniqWith,
+import {applyRuleSet, makerule} from "../common/validationCommon";
+import {map, uniqWith,
     flatten, filter} from "lodash/fp";
-import {$, isSomething, insensitiveEquals,
-    isNonEmptyString, none, isNothing, isArrayOfString} from "../common";
-import {getNode} from "../templateApi/heirarchy";
+import {$, insensitiveEquals,
+    isNonEmptyString, all} from "../common";
 
 const userRules = allUsers => [
     makerule("name", "username must be set",
@@ -12,13 +10,20 @@ const userRules = allUsers => [
     makerule("accessLevels", "user must have at least one access level",
         u => u.accessLevels.length > 0),
     makerule("name", "username must be unique",
-        u => filter(u2 => insensitiveEquals(u2,u))(allUsers).length > 1),
+        u => filter(u2 => insensitiveEquals(u2.name,u.name))
+                   (allUsers).length === 1),
     makerule("accessLevels", "access levels must only contain stings",
         u => all(isNonEmptyString)(u.accessLevels))
 ];
 
-export const validateUser = app => (allusers, user) => {
+export const validateUser = () => (allusers, user) => 
+    applyRuleSet(userRules(allusers))(user);
 
-
-
-};
+export const validateUsers = app => (allUsers) => 
+    $(allUsers, [
+        map(l => validateUser(app)(allUsers, l)),
+        flatten,
+        uniqWith((x,y) => x.field === y.field
+                        && x.item === y.item
+                        && x.error === y.error)
+    ]);
--- test/authApi.createUser.spec.js
+++ test/authApi.createUser.spec.js
@@ -28,8 +28,50 @@ describe("getNewUser", () => {
     });
 });
 
-describe.skip("validateUser", () => {
+describe("validateUsers", () => {
 
+    const validUser = (authApi) => {
+        const u = authApi.getNewUser();
+        u.name = "bob";
+        u.accessLevels = ["admin"];
+        u.enabled = true;
+        return u;
+    };
+
+    it("should not return errors for valid user", async () => {
+        const {authApi} = await setupAppheirarchy(basicAppHeirarchyCreator_WithFields);
+        const user = validUser(authApi);
+        const errs = authApi.validateUser([user], user);
+        expect(errs).toEqual([]);
+    });
+
+    it("should have error when username is not set", async () => {
+        const {authApi} = await setupAppheirarchy(basicAppHeirarchyCreator_WithFields);
+        const user = validUser(authApi);
+        user.name = "";
+        const errs = authApi.validateUser([user], user);
+        expect(errs.length).toBe(1);
+        expect(errs[0].field).toBe("name");
+    });
+
+    it("should have error when duplicate usernames", async () => {
+        const {authApi} = await setupAppheirarchy(basicAppHeirarchyCreator_WithFields);
+        const user1 = validUser(authApi);
+        const user2 = validUser(authApi);
+        const errs = authApi.validateUser([user1, user2], user1);
+        expect(errs.length).toBe(1);
+        expect(errs[0].field).toBe("name");
+    });
+
+    it("should have error when no access levels", async () => {
+        const {authApi} = await setupAppheirarchy(basicAppHeirarchyCreator_WithFields);
+        const user = validUser(authApi);
+        user.accessLevels = [];
+        const errs = authApi.validateUser([user], user);
+        expect(errs.length).toBe(1);
+        expect(errs[0].field).toBe("accessLevels");
+    });
+    
 });
 
 describe.skip("createUser", () => {
