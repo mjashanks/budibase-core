e642243b83f1044c6006ae1204a48dc014e7400b
marblekirby <daniel_loudon@hotmail.com>
2019-06-12 00:03:44 +0100

    broken stuff for review

--- src/appInitialise/initialiseData.js
+++ src/appInitialise/initialiseData.js
@@ -1,35 +1,59 @@
-import {configFolder, appDefinitionFile, $} from "../common";
-import {TRANSACTIONS_FOLDER} from "../transactions/transactionsCommon";
-import {AUTH_FOLDER, USERS_LIST_FILE, ACCESS_LEVELS_FILE} from "../authApi/authCommon";
-import {initialiseRootCollections} from "../collectionApi/initialise";
-import {initialiseIndex} from "../indexing/initialiseIndex";
-import {getFlattenedHierarchy, isGlobalIndex} from "../templateApi/heirarchy";
-import {filter} from "lodash/fp";
+import { configFolder, appDefinitionFile, $ } from "../common";
+import { TRANSACTIONS_FOLDER } from "../transactions/transactionsCommon";
+import { AUTH_FOLDER, USERS_LIST_FILE, ACCESS_LEVELS_FILE } from "../authApi/authCommon";
+import { initialiseRootCollections } from "../collectionApi/initialise";
+import { initialiseIndex } from "../indexing/initialiseIndex";
+import { getFlattenedHierarchy, isGlobalIndex, isSingleRecord } from "../templateApi/heirarchy";
+import { filter } from "lodash/fp";
+import { _save } from "../recordApi/save";
+import { getNew } from "../recordApi/getNew";
 
-export const initialiseData = async (datastore, applicationDefinition) => {
+export const initialiseData = async(datastore, applicationDefinition) => {
     await datastore.createFolder(configFolder);
     await datastore.createJson(appDefinitionFile, applicationDefinition);
 
     await initialiseRootCollections(datastore, applicationDefinition.heirarchy);
     await initialiseRootIndexes(datastore, applicationDefinition.heirarchy);
+
+    await initialiseRootSingleRecords(datastore, applicationDefinition.heirarchy);
+
     await datastore.createFolder(TRANSACTIONS_FOLDER);
 
     await datastore.createFolder(AUTH_FOLDER);
 
     await datastore.createJson(USERS_LIST_FILE, []);
 
-    await datastore.createJson(ACCESS_LEVELS_FILE, {version:0,levels:[]});
+    await datastore.createJson(ACCESS_LEVELS_FILE, { version: 0, levels: [] });
 }
 
-const initialiseRootIndexes = async (datastore, heirarchy) => {
+const initialiseRootIndexes = async(datastore, heirarchy) => {
 
     const flatheirarchy = getFlattenedHierarchy(heirarchy);
     const globalIndexes = $(flatheirarchy, [
         filter(isGlobalIndex)
     ]);
 
-    for(let index of globalIndexes) {
-        if(!await datastore.exists(index.nodeKey()))
+    for (let index of globalIndexes) {
+        if (!await datastore.exists(index.nodeKey()))
             await initialiseIndex(datastore, "", index);
-    } 
+    }
+}
+
+const initialiseRootSingleRecords = async(datastore, hierachy) => {
+
+    const flatheirarchy = getFlattenedHierarchy(hierachy);
+    const singleRecords = $(flatheirarchy, [
+        filter(isSingleRecord)
+    ]);
+
+    for (let record of singleRecords) {
+        const result = getNew({ datastore: datastore, hierarchy: appDefinition.hierarchy })
+            (record.nodeKey(),
+                record.name
+            );
+
+        _save({ datastore: datastore, hierarchy: appDefinition.hierarchy },
+            result
+        );
+    }
 }
